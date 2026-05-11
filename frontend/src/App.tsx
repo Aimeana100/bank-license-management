897f1ac1dc6b30934ed0241dae55858ce06d8615
Aimeana <aimeanathole@gmail.com>
2026-05-11 11:24:27 +0200

    feat: initial frontend setup
    - setup frontend with react.TS
    - initiated tailwind css as styling framework

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+        </div>
+        <div>
+          <h1 >License application and approval Pratform</h1>
+
+        </div>
+      </section>
+
+
+    </>
+  )
+}
+
+export default App
